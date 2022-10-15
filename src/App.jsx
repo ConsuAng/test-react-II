4cd116d9fecbed2dbc365eb57a148ac14274a57b
Consue <c.anguitasalinas@gmail.com>
2022-10-15 00:03:15 -0300

    feat: routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { ContextProvider } from './context/Context';
+import Cart from './pages/Cart';
+import Detail from './pages/Detail';
+import Home from './pages/Home';
 function App() {
 
   return (
     <div className="App">
+      <BrowserRouter>
+        <ContextProvider>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/pizza/:id' element={<Detail/>}/>
+            <Route path='/carrito' element={<Cart />} />
+          </Routes>
+        </ContextProvider>
+      </BrowserRouter>
     </div>
   )
 }
