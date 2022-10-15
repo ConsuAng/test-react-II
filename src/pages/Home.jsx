0116051c777d7fd80f50296c593179e7ef4102b2
Consue <c.anguitasalinas@gmail.com>
2022-10-15 00:01:50 -0300

    feat: Home and card component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import Context from "../context/Context";
+import Banner from "../components/Banner";
+import Card from "../components/Card";
+
+export default function Home() {
+  const { pizzas } = useContext(Context);
+
+  return (
+    <>
+    <Banner />
+      <div className="flex flex-wrap gap-5 justify-center pt-5">
+        {pizzas.map((pizza, i) => (
+          <Card key={i} pizza={pizza} />
+        ))}
+      </div>
+    </>
+  )
+}
