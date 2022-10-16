c37fc1c3ab4cca3d5dee70e5510dccfe10c5f37b
Consue <c.anguitasalinas@gmail.com>
2022-10-16 15:51:56 -0300

    🐛 fix: height in cards container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,11 +9,15 @@ export default function Home() {
   return (
     <>
       <Banner />
-      <div className="flex flex-wrap gap-5 justify-center pt-5">
-        {pizzas.map((pizza, i) => (
-          <Card key={i} pizza={pizza} />
-        ))}
+      <div className="min-h-[calc(100vh-264px)] flex justify-center items-center">
+        <div className="flex flex-wrap gap-5 justify-center pt-5 ">
+          {pizzas.map((pizza, i) => (
+            <Card key={i} pizza={pizza} />
+          ))}
+        </div>
       </div>
-    </> 
+    </>
   )
 }
+
+/* 160 + 48 +   56*/
