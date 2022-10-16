6e38ca0245e5e95b31bd777c2bb71d8e2b989476
Consue <c.anguitasalinas@gmail.com>
2022-10-16 15:51:09 -0300

    🐛 fix: Container height

--- src/layout/Container.jsx
+++ src/layout/Container.jsx
@@ -1,6 +1,6 @@
 export default function Container({children}) {
   return(
-    <div className="flex flex-col items-center justify-center h-[calc(100vh-104px)]">
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-104px)]">
       { children }
     </div>
   )
